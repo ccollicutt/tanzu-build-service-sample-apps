1fd7b753858b1bf495514921f24b202937fe9a2d
curtis <curtis@serverascode.com>
2021-04-06 14:43:15 -0400

    add wavefront

--- sample-apps/nodejs/server.js
+++ sample-apps/nodejs/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
+// If running in kubernetes, provide proxy location in env variable
+const wavefrontProxy = process.env.WAVEFRONT_PROXY || "localhost"
+const metrics = require('wavefrontmetrics');
+const registry = new metrics.Registry();
+// Report to a Wavefront proxy
+const proxyReporter = new metrics.WavefrontProxyReporter(registry, "wavefront.nodejs.proxy", wavefrontProxy, 2878, { "tag0": "default", "source": "tbs-sample-app-nodejs"});
+proxyReporter.start(5000);
 
 const app = express();
 
 app.get('/', (request, response) => {
+  // Counter with metric level tags
+  c.inc();
+
   response.send(`<!DOCTYPE html>
 <html>
   <head>
@@ -14,5 +24,10 @@ app.get('/', (request, response) => {
   </body>
 </html>`);
 });
+
+
+// setup the wavefront metric
+let c = new metrics.Counter();
+registry.addTaggedMetric("request.counter", c, {"key1":"val1"});
 
 app.listen(port);
